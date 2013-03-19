489063041f801ec5f69cb3da5bfdad3eb6633984
Graham King <graham@gkgk.org>
2013-03-18 22:04:18 -0700

    Progress on python version of pysse

--- pypysse.py
+++ pypysse.py
@@ -3,64 +3,164 @@ import socket
 import select
 import os
 import sys
+import redis
+import threading
+
+import time
 
 HEAD_TMPL = "HTTP/1.1 200 OK\nCache-Control: no-cache\nContent-Type: text/event-stream\nConnection: keep-alive\nAccess-Control-Allow-Origin: *\n\n"
 
-
-def start_sock():
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setblocking(0)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("127.0.0.1", 1234))
-    sock.listen(5)
-    return sock
+REDIS_QUEUE = "pysse"
 
 
-def start_epoll(sockfd):
+def start_redis_thread(list_name, p_write):
+    """
+        Start redis in a separate thread, copying input from redis
+        onto pipe fd.
 
-    ep = select.epoll()
-    ep.register(sockfd, select.EPOLLIN)
-    return ep
+        list_name: Redis list to take commands from
+        p_write: File descriptor (pipe) for writing redis messages to.
+    """
+
+    r = redis.StrictRedis(host="127.0.0.1")
+
+    def inner():
+        while 1:
+            _, val = r.blpop([list_name])
+            os.write(p_write, val)
+
+    t = threading.Thread(target=inner, name='pysse redis')
+    t.daemon = 1
+    t.start()
 
 
-def acceptnew(sock, ep):
+class Server(object):
+    """EventSource server.
+    """
 
-    conn, _ = sock.accept()
-    conn.setblocking(0)
-    ep.register(conn.fileno(), select.EPOLLIN)
+    def __init__(self):
 
-    conn.send(HEAD_TMPL)
+        self.sock = self.start_sock()
+        self.ep = self.start_epoll()
 
-    return conn
+        self.p_read = self.pipe_from_redis()
 
+        self.conns = {}                 # Active connections
+        self.out_message = None         # Message to send out
 
-def main_loop(sock, ep):
+    def start_sock(self):
 
-    sockfd = sock.fileno()
-    conns = []
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(0)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("127.0.0.1", 1234))
+        sock.listen(5)
+        return sock
 
-    while 1:
+    def start_epoll(self):
 
-        events = ep.poll(maxevents=100)
+        ep = select.epoll()
+        ep.register(self.sock.fileno(), select.EPOLLIN)
+        return ep
 
-        for event in events:
-            print(event)
+    def pipe_from_redis(self):
+        """Create a pipe and connect to a thread which reads from
+        a Redis queue.
+        """
+        p_read, p_write = os.pipe()
+        self.ep.register(p_read, select.EPOLLIN)
+
+        start_redis_thread(REDIS_QUEUE, p_write)
+
+        return p_read
+
+    def acceptnew(self):
+        """Accept a new client connection.
+        """
+
+        conn, _ = self.sock.accept()
+        print("Accepted: %d" % conn.fileno())
+        conn.setblocking(0)
+
+        self.ep.register(conn.fileno(), select.EPOLLIN)
+        conn.send(HEAD_TMPL)
+
+        self.conns[conn.fileno()] = conn
+
+    def get_redis_command(self):
+        """Read command from redis pipe.
+        """
+        self.out_message = os.read(self.p_read, 1024)
+
+    def listen_for_out(self):
+        """Listen for EPOLLOUT readiness on all the connections
+        in conn_fds.
+        ep: epoll object.
+        """
+        for fd in self.conns.keys():
+            print("listen_for_out: %d" % fd)
+            try:
+                self.ep.modify(fd, select.EPOLLIN | select.EPOLLOUT)
+            except IOError:
+                print("IOError ep.modify on %d" % fd)
+
+    def event_generator(self):
+        """Yield epoll events, forever. Blocks if no events.
+        """
+        while 1:
+            for event in self.ep.poll(maxevents=100):
+                yield event
+            time.sleep(0.5)
+
+    def run(self):
+        """main_loop. Runs forever"""
+
+        sockfd = self.sock.fileno()
+
+        for event in self.event_generator():
+            print("----- {}".format(event))
+
             efd, etype = event
             if etype == select.EPOLLIN:
+                print("EPOLLIN: %d" % efd)
 
-                if efd == sockfd:
-                    conns.append(acceptnew(sock, ep))
-                else:
-                    print("Other fd: %d" % efd)
+                if efd == sockfd:   # New client connection
+
+                    self.acceptnew()
+
+                elif efd == self.p_read: # New command from Redis
+
+                    self.get_redis_command()
+                    self.listen_for_out()
+
+                else:   # From a client socket
+
+                    inp = os.read(efd, 1024)
+                    if not inp: # EOF
+                        self.conns[efd].close()
+                        del self.conns[efd]
+                        print("Closed %d" % efd)
+
+                    else:
+                        print(inp)
+
+            elif etype == select.EPOLLOUT:
+                print("EPOLLOUT: %d" % efd)
+
+                if not self.out_message:
+                    continue
+
+                num_wrote = os.write(efd, self.out_message)
+                print("Wrote %d bytes" % num_wrote)
+                self.ep.modify(efd, select.EPOLLIN)
+
+            print("Open conns: {}".format(self.conns.keys()))
 
 
 def main():
 
-    sock = start_sock()
-    sockfd = sock.fileno()
-    ep = start_epoll(sockfd)
-    main_loop(sock, ep)
+    server = Server()
+    server.run()
 
     return 0
 
